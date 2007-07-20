3661aa206d6a9735650a3a6db1d76c5bbb237f9a
richard.m.tew <none@none>
2007-07-20 17:51:35 +0000

    Cleaned up the code to no longer be dependent on the internals of the stacklesssocket module.  It also properly uses socket.recv, taking a returned empty string to mean disconnection.

--- examples/networking/mud.py
+++ examples/networking/mud.py
@@ -7,168 +7,187 @@
 # Feel free to email me with any questions, comments, or suggestions for
 # improvement.
 #
-# - Could possibly be even simpler if the TelnetConnection class were removed
-#   and read/readline were done locally to the MUD code.  Then disconnection
-#   detection could be built into the stacklesssocket.dispatcher class.
-#
 
+import sys, traceback, weakref, logging
 import stackless
-import stacklesssocket as socket
-import random, time
-import traceback
 
-class TelnetConnectionWrapper(socket.stacklesssocket):
-    connectionID = None
-    disconnectChannel = None
+# Monkeypatch in the 'stacklesssocket' module, so we get blocking sockets
+# which are Stackless compatible.  This example code will avoid any use of
+# the Stackless sockets except through normal socket usage.
+import stacklesssocket
+sys.modules["socket"] = stacklesssocket
+import socket
 
-    def __init__(self, sock):
-        self.sock = sock
-        self.dispatcher = TelnetConnection(sock)
 
-    def close(self):
-        # Notify the server.
-        if self.disconnectChannel is not None:
-            # This will not block.  The channel is set to schedule
-            # receivers and return to the sender immediately.
-            self.disconnectChannel.send(self)
-            self.disconnectChannel = None
-        # Do the standard socket closure handling.  But we can't
-        # call the underlying function as it doesn't exist on the
-        # superclass, and trying to call it there anyway won't get it
-        # routed through getattr.
-        self.dispatcher.close()
+class RemoteDisconnectionError(StandardError):
+    pass
 
-class TelnetConnection(socket.dispatcher):
-    echo = False
 
-    def read(self): # TELNET
-        ret = self.recvChannel.receive()
-        if self.echo:
-            if ret == '\x08':
-                self.send(ret+" ")
-            self.send(ret)
-        return ret
+class User:
+    def __init__(self, connection):
+        self.connection = connection
 
-    def readline(self): # TELNET
-        buf = self.readBufferString
+        # The tasklet will hold a reference to the user keeping the instance
+        # alive as long as it is handling commands.
+        stackless.tasklet(self.Run)()
 
+    def Run(self):
+        global server
+
+        # Notify the server that a user is connected.
+        server.RegisterUser(self)
+        logging.info("Connected %d from %s", id(self), self.connection.clientAddress)
+
+        try:
+            while self.HandleCommand():
+                pass
+
+            self.OnUserDisconnection()
+        except RemoteDisconnectionError:
+            self.OnRemoteDisconnection()
+            self.connection = None
+        except:
+            traceback.print_exc()
+        finally:
+            if self.connection:
+                self.connection.Disconnect()
+                self.connection = None
+
+    def HandleCommand(self):
+        self.connection.Write("> ")
+        line = self.connection.ReadLine()
+        words = [ word.strip() for word in line.strip().split(" ") ]
+        verb = words[0]
+
+        if verb == "look":
+            userList = server.ListUsers()
+            self.connection.WriteLine("There are %d users connected:" % len(userList))
+            self.connection.WriteLine("Name\tHost\t\tPort")
+            self.connection.WriteLine("-" * 40)
+            for user in userList:
+                host, port = user.connection.clientAddress
+                self.connection.WriteLine("Unknown\t"+ str(host) +"\t"+ str(port))
+        elif verb == "say":
+            line = line[4:]
+            secondPartyPrefix = "Someone says: "
+            for user in server.ListUsers():
+                if user is self:
+                    prefix = "You say: "
+                else:
+                    prefix = secondPartyPrefix
+                user.connection.WriteLine(prefix + "\"%s\"" % line)
+        elif verb == "quit":
+            return False
+        elif verb == "help":
+            self.connection.WriteLine("Commands:")
+            for verb in [ "look", "say", "quit", "help" ]:
+                self.connection.WriteLine("  "+ verb)
+        else:
+            self.connection.WriteLine("Unknown command.  Type 'help' to see a list of available commands.")
+
+        return True
+
+    def OnRemoteDisconnection(self):
+        # This is no longer valid, clear it.
+        logging.info("Disconnected %d (remote)", id(self))
+        self.connection = None
+
+    def OnUserDisconnection(self):
+        logging.info("Disconnected %d (local)", id(self))
+
+class Connection:
+    disconnected = False
+
+    def __init__(self, clientSocket, clientAddress):
+        self.clientSocket = clientSocket
+        self.clientAddress = clientAddress
+
+        self.readBuffer = ""
+
+        self.userID = id(User(self))
+
+    def Disconnect(self):
+        if self.disconnected:
+            raise RuntimeError("Unexpected call")
+        self.disconnected = True
+        self.clientSocket.close()
+
+    def Write(self, s):
+        self.clientSocket.send(s)
+
+    def WriteLine(self, s):
+        self.Write(s +"\r\n")
+
+    def ReadLine(self):
+        global server
+    
+        s = self.readBuffer
         while True:
-            if buf.find('\r\n') > -1:
-                i = buf.index('\r\n')
-                ret = buf[:i+2]
-                self.readBufferString = buf[i+2:]
-                while '\x08' in ret:
-                    i = ret.index('\x08')
+            # If there is a CRLF in the text we have, we have a full
+            # line to return to the caller.
+            if s.find('\r\n') > -1:
+                i = s.index('\r\n')
+                # Strip the CR LF.
+                line = s[:i]
+                self.readBuffer = s[i+2:]
+                while '\x08' in line:
+                    i = line.index('\x08')
                     if i == 0:
-                        ret = ret[1:]
+                        line = line[1:]
                     else:
-                        ret = ret[:i-1]+ret[i+1:]
-                return ret
+                        line = line[:i-1] + line[i+1:]
+                return line
 
-            buf += self.read()
+            # An empty string indicates disconnection.
+            v = self.clientSocket.recv(1000)
+            if v == "":
+                self.disconnected = True
+                raise RemoteDisconnectionError
+            s += v
 
-def RunServer(host, port):
-    listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listenSocket.bind((host, port))
-    listenSocket.listen(5)
 
-    # Connecting sockets should be wrapped in TelnetConnection, rather than
-    # stacklesssocket.dispatcher (the default wrapper).
-    def wrap_accept_socket(currentSocket):
-        return TelnetConnectionWrapper(currentSocket)
-    listenSocket.wrap_accept_socket = wrap_accept_socket
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+                
+        self.userIndex = weakref.WeakValueDictionary()
 
-    nextConnectionID = 1
-    infoByConnectionID = {}
-    disconnectChannel = stackless.channel()
-    disconnectChannel.preference = 1
-    stackless.tasklet(MonitorDisconnections)(disconnectChannel, infoByConnectionID)
+        stackless.tasklet(self.Run)()
 
-    print "Accepting connections on", host, port
-    try:
-        while listenSocket.accepting:
-            clientSocket, clientAddress = listenSocket.accept()
-            clientSocket.disconnectChannel = disconnectChannel
-            clientSocket.connectionID = nextConnectionID
-            print "Received connection #", clientSocket.connectionID, "from", clientAddress
-            infoByConnectionID[clientSocket.connectionID] = clientAddress, clientSocket
-            nextConnectionID += 1
-            stackless.tasklet(MonitorIncomingConnection)(clientSocket, clientAddress, infoByConnectionID)
-            stackless.schedule()
-    except socket.error:
-        print "RunServer.error"
-        traceback.print_exc()
-    print "RunServer.exit"
+    def Run(self):
+        listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listenSocket.bind((self.host, self.port))
+        listenSocket.listen(5)
 
-def MonitorDisconnections(disconnectChannel, infoByConnectionID):
-    print "MonitorDisconnections"
-    try:
-        while True:
-            clientSocket = disconnectChannel.receive()
-            print "Received disconnection of #", clientSocket.connectionID, "from", infoByConnectionID[clientSocket.connectionID][0]
-            del infoByConnectionID[clientSocket.connectionID]
-    except socket.error:
-        print "MonitorDisconnections.error"
-        traceback.print_exc()
-    print "MonitorDisconnections.exit"
+        logging.info("Accepting connections on %s %s", self.host, self.port)
+        try:
+            while listenSocket.accepting:
+                clientSocket, clientAddress = listenSocket.accept()
+                Connection(clientSocket, clientAddress)
+                stackless.schedule()
+        except socket.error:
+            traceback.print_exc()
 
-def MonitorIncomingConnection(clientSocket, clientAddress, infoByConnectionID):
-    clientSocket.send("Welcome to a basic Stackless Python MUD server.\r\n")
-    try:
-        while clientSocket.connected:
-            clientSocket.send("> ")
-            line = clientSocket.readline()[:-2].strip()
-            words = [ word.strip() for word in line.split(" ") ]
-            verb = words[0]
+    def RegisterUser(self, user):
+        self.userIndex[id(user)] = user
+        
+    def ListUsers(self):
+        return [ v for v in self.userIndex.itervalues() ]
 
-            if verb == "look":
-                clientSocket.send("There are %d users connected:\r\n" % len(infoByConnectionID))
-                clientSocket.send("Name\tHost\t\tPort\r\n")
-                clientSocket.send("-" * 40 +"\r\n")
-                for clientAddress2, clientSocket2 in infoByConnectionID.itervalues():
-                    clientSocket.send("Unknown\t"+ str(clientAddress2[0]) +"\t"+ str(clientAddress2[1]) +"\r\n")
-            elif verb == "say":
-                line = line[4:]
-                secondPartyPrefix = "Someone says: "
-                for clientAddress2, clientSocket2 in infoByConnectionID.itervalues():
-                    if clientSocket2 is clientSocket:
-                        prefix = "You say: "
-                    else:
-                        prefix = secondPartyPrefix
-                    clientSocket2.send(prefix + "\"%s\"\r\n" % line)
-            elif verb == "quit":
-                clientSocket.close()
-            elif verb == "help":
-                clientSocket.send("Commands:\r\n")
-                for verb in [ "look", "say", "quit", "help" ]:
-                    clientSocket.send("  "+ verb +"\r\n")
-            else:
-                clientSocket.send("Unknown command.  Type 'help' to see a list of available commands.\r\n")
 
-            stackless.schedule()
-    except socket.error:
-        print "MonitorIncomingConnection.socket.error"
-        traceback.print_exc()
+def Run(host, port):
+    global server
+    server = Server(host, port)
+    while 1:
+        stackless.run()
 
 if __name__ == "__main__":
-    host = "127.0.0.1"
-    port = 3000
-
-    # We do not want this to start as we will run it ourselves.
-    socket.managerRunning = True
-
-    t = stackless.tasklet(RunServer)(host, port)
-    # ManageSockets will exit if there are no sockets existing, so
-    # by running this tasklet once, we will get the listen socket in
-    # place before we invoke ManageSockets to run.
-    t.run()
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(message)s')
 
     try:
-        socket.ManageSockets()
+        Run("127.0.0.1", 3000)
     except KeyboardInterrupt:
-        print "** Detected ctrl-c in the console"
-    except:
-        print "main error"
-        traceback.print_exc()
-    print "EXIT"
+        logging.info("Server manually stopped")
