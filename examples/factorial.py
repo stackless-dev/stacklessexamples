92182cff2f893053fce4ff6a3190099d879c5522
Elliot Ford <elliot.ford@hotmail.co.uk>
2022-11-11 11:44:30 +0000

    PEP8-compliant blank lines around functions
    See this section: https://pep8.org/#blank-lines
    
    "Surround top-level function and class definitions with two blank lines."

--- examples/factorial.py
+++ examples/factorial.py
@@ -19,17 +19,22 @@
 import stackless
 import time
 
+
 def call_wrapper(f, args, kwargs, result_ch):
     result_ch.send(f(*args, **kwargs))
+
 
 def call(f, *args, **kwargs):
     result_ch = stackless.channel()
     stackless.tasklet(call_wrapper)(f, args, kwargs, result_ch)
     return result_ch.receive()
+
+
 def factorial(n):
     if n <= 1:
         return 1
     return n * call(factorial, n-1)
+
 
 st = time.time()
 
